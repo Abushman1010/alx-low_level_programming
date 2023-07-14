f9184a010b1cfd24d27aa6dbf5ca75e719f45080
Abush Man <manabush06@gmail.com>
2023-07-14 11:15:37 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Entry point of the program
+*Discription: 'Print alphabet lowercase'
+*Return: 0 on successful execution
+*/
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
