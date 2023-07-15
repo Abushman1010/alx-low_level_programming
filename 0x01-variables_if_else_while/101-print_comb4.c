565cbbe8d5caecd0a45592b2d707dba3e1e3f7f8
Abush Man <manabush06@gmail.com>
2023-07-15 07:57:18 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,27 +6,16 @@
 */
 int main(void)
 {
-	int n, m, l;
+	int d;
 
-	for (n = 48; n < 58; n++)
+	for (d = '0'; d <= '9'; d++)
 	{
-		for (m = 49; m < 58; m++)
-		{
-			for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-				        {
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
+	putchar(d);
+	if (d != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
