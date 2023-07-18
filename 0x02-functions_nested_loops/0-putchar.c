0a545636a797350cbe10867fa9a9265ffb80bfbe
Abush Man <manabush06@gmail.com>
2023-07-18 06:56:45 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
-*main - Entry point of the program
-*Discription: 'print string using putchar'
-*Return: 0 on successful execution
-*/
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
+
